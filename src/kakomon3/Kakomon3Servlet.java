6c60ef31a82e7de8577451729c6ea5c91b167d1e
t.yoshizawa <t.yoshizawa@jc-21.ac.jp>
2014-12-02 18:28:59 +0900

    Google Cloud Storageから画像ファイルを引っ張ってくるように変更

--- src/kakomon3/Kakomon3Servlet.java
+++ src/kakomon3/Kakomon3Servlet.java
@@ -3,11 +3,30 @@ package kakomon3;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @SuppressWarnings("serial")
 public class Kakomon3Servlet extends HttpServlet {
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Servlet and JSP world");
+		resp.setContentType("text/html");
+		resp.getWriter().println("<h1>Hello, Servlet and JSP world</h1>");
+
+		String s = "https://storage.cloud.google.com/kakomondb/ap/APH241003.png";
+
+		resp.getWriter().print("<hr>");
+		resp.getWriter().print("<img src='" + s + "' width=1000>");
+
+		
+		
 	}
 }
