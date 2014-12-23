7deb327568e2f5ee60308185dee7c3252185c730
t.yoshizawa <t.yoshizawa@jc-21.ac.jp>
2014-12-24 08:44:04 +0900

    コメント化した不要なプログラムを削除

--- src/kakomon3/Kakomon3TagServlet.java
+++ src/kakomon3/Kakomon3TagServlet.java
@@ -1,9 +1,7 @@
 package kakomon3;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,8 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.sun.corba.se.impl.monitoring.MonitoredObjectImpl;
 
 import kakomon3.jdo.Genre;
 import kakomon3.jdo.Mondai;
@@ -28,16 +24,9 @@ public class Kakomon3TagServlet extends HttpServlet {
 			throws IOException, ServletException {
 
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-
-		// List<Genre> list2 = Genre.findAll(pm);
 		Map<String, String> mapGenre = Genre.getMapAll(pm);
-
-		Map<String,Tag> mapTag = Tag.getMapAll(pm);
 		List<Tag> allTag = Tag.findAll(pm);
-
-		List<Mondai> list = Mondai.findAll(pm);
 		Map<String, Mondai> mapMondai = Mondai.getMapAll(pm);
-		
 		List<String[]> tagList = new ArrayList<String[]>();
 
 		for (Tag t : allTag) {
@@ -48,12 +37,14 @@ public class Kakomon3TagServlet extends HttpServlet {
 				List<String> tags = mondai.getTags();
 
 				String[] s = new String[4 + tags.size()];
+				
 				s[0] = tagName;
 				s[1] = mondai.getURL();
 				s[2] = mapGenre.get(mondai.getGenre());
 				s[3] = mondai.getComment();
 				s[4] = "";
-				List<String> tList = mondai.getTags();
+				
+//				List<String> tList = mondai.getTags();
 				for (int i=0 ; i<tags.size() ;i++) {
 					s[4+i] = tags.get(i);
 				}
