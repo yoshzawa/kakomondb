97e094bfe658a196db0017b383a88c0eca72d763
yoshzawa <yoshzawa@192.168.11.3>
2014-12-31 10:50:12 +0900

    kakomon3.quizパッケージの作成

---
+++ src/kakomon3/quiz/KakomonQuizListServlet.java
@@ -0,0 +1,64 @@
+package kakomon3.quiz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kakomon3.jdo.Genre;
+import kakomon3.jdo.Mondai;
+import kakomon3.jdo.MondaiImage;
+import kakomon3.jdo.PMF;
+import kakomon3.jdo.Tag;
+
+@SuppressWarnings("serial")
+public class KakomonQuizListServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
+
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+
+		Map<String, String> mapGenre = Genre.getMapAll(pm);
+
+		List<Mondai> list = Mondai.findAll(pm);
+		
+		Map<String, MondaiImage> mapMondaiImage = MondaiImage.getMapAll(pm);
+
+
+		List<String[]> mondaiList = new ArrayList<String[]>();
+		for (Mondai m : list) {
+			List<String> tagList = m.getTags();
+			int tagLength = tagList.size();
+			
+			String[] s = new String[5+tagLength];
+			s[0] = m.getId();
+			String id = m.getId();
+			System.out.println(id);
+			s[1] = mapMondaiImage.get(id).getURL();
+			s[2] = m.getComment();
+			s[3] = mapGenre.get(m.getGenre());
+			s[4] = m.getKotae().toString();
+			for(int i=0 ; i<tagLength ; i++){
+				s[5+i]=tagList.get(i);
+			}
+			mondaiList.add(s);
+		}
+
+		req.setAttribute("mondaiList", mondaiList);
+		pm.close();
+
+		RequestDispatcher rd = req
+				.getRequestDispatcher("/WEB-INF/jsp/mondai.jsp");
+		rd.forward(req, resp);
+	}
+}
