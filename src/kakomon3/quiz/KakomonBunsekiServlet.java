32e61727cb407d4d1584ba2a42519280b702329e
yoshzawa <yoshzawa@192.168.11.4>
2015-01-20 00:13:15 +0900

    結果画面でGenre一覧を表示するようにしたが、genreごとに正解・不正解した問題のリストを作成するようにしたい

--- src/kakomon3/quiz/KakomonBunsekiServlet.java
+++ src/kakomon3/quiz/KakomonBunsekiServlet.java
@@ -1,7 +1,11 @@
 package kakomon3.quiz;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.jdo.PersistenceManager;
 import javax.servlet.RequestDispatcher;
@@ -10,7 +14,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kakomon3.jdo.Genre;
 import kakomon3.jdo.Kaitou;
+import kakomon3.jdo.Mondai;
 import kakomon3.jdo.PMF;
 
 import com.google.appengine.api.users.User;
@@ -30,14 +36,55 @@ public class KakomonBunsekiServlet extends HttpServlet {
 			UserService service = UserServiceFactory.getUserService();
 			User user = service.getCurrentUser();
 			List<Kaitou> list = Kaitou.getListByUser(pm, user);
+			Map<String, Mondai> mondais = Mondai.getMap(pm);
+
+			List<Genre> genreList = Genre.getList(pm);
+			Map<String , Integer> genreWinCount = new HashMap<String, Integer>();
+			Map<String , Integer> genreLoseCount = new HashMap<String, Integer>();
+			Map<String , Integer> genreAllCount = new HashMap<String, Integer>();
+			for(Genre s : genreList){
+				String gId = s.getId();
+				genreWinCount.put(gId, 0);
+				genreLoseCount.put(gId, 0);
+				genreAllCount.put(gId, s.getMondais().size());
+			}
 
 			int seikai = 0;
 			int machigai = 0;
 
 			for (Kaitou k : list) {
-				seikai += (k.isSeikai() ? 1 : 0);
-				machigai += (k.isSeikai() ? 0 : 1);
+				String mId = k.getMondaiId();
+				String gId = mondais.get(mId).getGenre();
+				if(k.isSeikai() == true){
+					seikai ++;
+					int i=genreWinCount.get(gId);
+					i++;
+					genreWinCount.put(gId, i);
+				} else {
+					machigai++;
+					int i=genreLoseCount.get(gId);
+					i++;
+					genreLoseCount.put(gId, i);
+				}
 			}
+			List<String[]> genreResult = new ArrayList<>();
+			
+			for(Genre s : genreList){
+				String gId = s.getId();
+				String ss[]=new String[4];
+				ss[0] = s.getName();
+				ss[1] = 
+				genreWinCount.get(gId)+"";
+				ss[2] = 
+				genreLoseCount.get(gId)+"";
+				ss[3] = (
+				genreAllCount.get(gId) - 
+				genreWinCount.get(gId) -
+				genreLoseCount.get(gId)) + "";
+				
+				genreResult.add(ss);
+			}
+			req.setAttribute("genreResult", genreResult);
 
 			String[] s = new String[7];
 
--- war/WEB-INF/jsp/quiz/bunseki.jsp
+++ war/WEB-INF/jsp/quiz/bunseki.jsp
@@ -3,6 +3,8 @@
 
 <%
 String[] result = (String[])request.getAttribute("result");
+List<String[]> genreResult = (List<String[]>)request.getAttribute("genreResult");
+
 %>
 
 <h1>結果一覧</H1>
@@ -11,4 +13,17 @@ String[] result = (String[])request.getAttribute("result");
 <table border=1>
 <tr><th>正解</th><td><%= result[1] %>問</td></tr>
 <tr><th>不正解</th><td><%= result[2] %>問</td></tr>
+</table>
+
+<table border=1>
+<% for(String[] s : genreResult){
+%>
+<tr>
+<th><%= s[0] %></th>
+<td><%= s[1] %></td>
+<td><%= s[2] %></td>
+<td><%= s[3] %></td>
+</tr>
+<% }
+%>
 </table>
