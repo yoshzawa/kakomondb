b8df64a110231a77aeaca084b4b1af4e45024b0c
t.yoshizawa <t.yoshizawa@jc-21.ac.jp>
2015-03-16 11:12:21 +0900

    PersonalDataの微調整

--- src/kakomon3/PersonalData.java
+++ src/kakomon3/PersonalData.java
@@ -1,7 +1,7 @@
 package kakomon3;
 
 public class PersonalData {
-	public static final String googleStorageBucket ="kakomondb";
+	public static final String googleStorageBucket = "kakomondb";
 	public static final String googleAnalyticsId = "UA-58680493-1";
 	public static final String appVersion = "3beta1 : kaiinなど追加した";
 	public static final String defaultGenreId = "1-01";
