71defbd203ee9582685f165235c0ebdbdf85e8f5
yoshzawa <yoshzawa@gmail.com>
2015-06-21 17:56:39 +0900

    3beta6でデプロイ

--- src/kakomon3/PersonalData.java
+++ src/kakomon3/PersonalData.java
@@ -3,6 +3,6 @@ package kakomon3;
 public class PersonalData {
 	public static final String googleStorageBucket = "kakomondb";
 	public static final String googleAnalyticsId = "UA-58680493-1";
-	public static final String appVersion = "3beta5 : TagをLowLevelApiで書き直した";
+	public static final String appVersion = "3beta6 : genreをLowLevelApiで書き直した。あといろいろ仕切り直し";
 	public static final String defaultGenreId = "1-01";
 }
